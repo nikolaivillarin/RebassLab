17c6d9fc9122c9bef94a74480ddf63f9a2d91bac
Nikolai Villarin <nikolai.villarin@gmail.com>
2020-02-02 15:37:59 -0500

    Checkpoint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import image from './content/photo.jpeg'
 import './App.css';
+import RebassCard from './rebass'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <RebassCard image={image} title="Bob" description="The Builder" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
